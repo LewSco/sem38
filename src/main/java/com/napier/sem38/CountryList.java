9690a03463b0c59c88bbcd82a6ad98aaafa241f1
Lewis Scott <Ec2078156@edinburghcollege.ac.uk>
2024-10-09 15:10:55 +0100

    made a new class for listing countries and set up a list for storing them

---
+++ src/main/java/com/napier/sem38/CountryList.java
@@ -0,0 +1,22 @@
+package com.napier.sem38;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountryList
+{
+
+    //list for storing the countries
+    List<String> _list;
+
+    public CountryList()
+    {
+        // we initialise the list
+        _list = new ArrayList<>();
+    }
+
+    public void CountyListByPop()
+    {
+
+    }
+}
