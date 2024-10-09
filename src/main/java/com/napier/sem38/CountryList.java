841fb82a55fbefc7bf1d8617bfb7163117584396
Lewis Scott <Ec2078156@edinburghcollege.ac.uk>
2024-10-09 21:26:58 +0100

    finished adding country list world query and function for displaying results. Implemented in main to test

--- src/main/java/com/napier/sem38/CountryList.java
+++ src/main/java/com/napier/sem38/CountryList.java
@@ -6,21 +6,52 @@ import java.util.List;
 
 public class CountryList
 {
+    List<String> _list; // list for storing the countries
+    Database _database;
 
-    //list for storing the countries
-    List<String> _list;
-    // stores the current query
-    String _query;
 
-    public CountryList()
+    public CountryList(Database database)
     {
         // we initialise the list
         _list = new ArrayList<>();
+        _database = database;
     }
 
 
-    public void CountyListByPop(ResultSet results)
+    public void GetWorldList()
     {
+        _list.clear();
 
+        String query =
+                "SELECT Name " +
+                        "FROM country " +
+                        "ORDER BY Population DESC";
+
+        ResultSet results = _database.Query(query);
+
+        try
+        {
+            while (results.next())
+            {
+                _list.add(results.getString("Name"));
+            }
+        }
+        catch(Exception exception)
+        {
+            // print error messages
+            System.out.println(exception.getMessage());
+            System.out.println("Error retrieving data from ResultSet!");
+        }
+
+    }
+
+    public void Display()
+    {
+        System.out.println("List of countries:");
+
+        for (String country : _list)
+        {
+            System.out.println(country);
+        }
     }
 }
--- src/main/java/com/napier/sem38/Main.java
+++ src/main/java/com/napier/sem38/Main.java
@@ -8,114 +8,19 @@ public class Main
 {
     public static void main(String[] args)
     {
-        // Create new Application
-        Main _main = new Main();
+
+        Database _database = new Database(); // initialise our database connection
+        CountryList _countryList = new CountryList(_database); // initialise the country list
 
         // Connect to database
-        _main.connect();
+        _database.connect();
+
+        // query the database for the worldList
+        _countryList.GetWorldList();
+
+        _countryList.Display(); // display our results
 
         // Disconnect from database
-        _main.disconnect();
-    }
-
-
-
-    /**
-     * Connection to MySQL database.
-     */
-    private Connection con = null;
-
-    /**
-     * Connect to the MySQL database.
-     */
-    public void connect()
-    {
-        try
-        {
-            // Load Database driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
-        }
-
-        int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
-            System.out.println("Connecting to database...");
-            try
-            {
-                // Wait a bit for db to start
-                Thread.sleep(30000);
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://database:3306/world?useSSL=false", "root", "example");
-                System.out.println("Successfully connected");
-                break;
-            }
-            catch (SQLException sqle)
-            {
-                System.out.println("Failed to connect to database attempt " + i);
-                System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
-                System.out.println("Thread interrupted? Should not happen.");
-            }
-        }
-    }
-
-    /**
-     * Disconnect from the MySQL database.
-     */
-    public void disconnect()
-    {
-        if (con != null)
-        {
-            try
-            {
-                // Close connection
-                con.close();
-            }
-            catch (Exception e)
-            {
-                System.out.println("Error closing connection to database");
-            }
-        }
-    }
-
-    // function for querying the database
-    public ResultSet Query(String query)
-    {
-        // set up a results set with a null value
-        ResultSet resultSet = null;
-
-        // try to query the database
-        try
-        {
-
-            if (query == null || query.isEmpty())
-                throw new Exception("Query statement is null or empty!");
-
-            // create sql statement
-            Statement stmt = con.createStatement();
-
-            // we query the database using the String parameter provided
-            resultSet = stmt.executeQuery(query);
-
-            if (resultSet == null)
-                throw new Exception("Query results returned null!");
-
-        }
-        catch (Exception exception) // if an error occurs then we
-        {
-            // print the error message
-            System.out.print(exception.getMessage());
-            System.out.print("Query Failed!"); // and tell the user the query failed
-        }
-
-        // we return the results set even if its null
-        return resultSet;
+        _database.disconnect();
     }
 }
