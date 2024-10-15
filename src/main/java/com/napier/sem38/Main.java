1e7d59417f3c5a517425356b429e20a32be487be
Lewis Scott <Ec2078156@edinburghcollege.ac.uk>
2024-10-15 10:00:26 +0100

    edited main to work with new methods

--- src/main/java/com/napier/sem38/Main.java
+++ src/main/java/com/napier/sem38/Main.java
@@ -9,11 +9,16 @@ public class Main {
      * Instantiates, runs and displays all of the currently existing classes.
      * @param args
      */
-    public static void main(String[] args) {
-        Database _database = new Database(); // Initialize database connection
-        CountryList _countryList = new CountryList(_database); // Initialize the country list
-        TopNPopCountries _topNPopCountries = new TopNPopCountries(_database); // Initialize top populated countries list
-        CountientListByPop _countientListByPop = new CountientListByPop(_database); // Initialize continent list
+    public static void main(String[] args)
+    {
+        // Initialize database connection
+        Database _database = new Database();
+
+        // Initialize the list
+        CountryList _countryList = new CountryList(_database);
+
+        int _number;
+        String _name;
 
         // Connect to database
         _database.connect();
@@ -21,16 +26,25 @@ public class Main {
         // Get world list ordered by population
         _countryList.GetWorldList();
 
-        // Get top N populated countries
-        _topNPopCountries.GetTopPopCountries(3);
-
-        // Get continent list by population (replace "Asia" with the desired continent)
-        _countientListByPop.getCountriesByPopulation("Asia");
-
-        // Display all results
+        // Display results
+        System.out.println("\nList of the worlds countries by population:");
         _countryList.Display();
-        _topNPopCountries.Display();
-        _countientListByPop.Display();
+
+        // Get world list ordered by population up to n number of countries
+        _number = 4;
+        _countryList.GetWorldList(_number);
+
+        // Display results
+        System.out.println("\nList of " + _number + " of the worlds countries by population:");
+        _countryList.Display();
+
+        // Get world list ordered by population up to n number of countries
+        _name = "Asia";
+        _countryList.GetContinentList(_name);
+
+        // Display results
+        System.out.println("\nList of countries in " + _name + " by population:");
+        _countryList.Display();
 
         // Disconnect from database
         _database.disconnect();
