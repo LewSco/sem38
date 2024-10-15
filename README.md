f953bd85a237251293f2493bf75078454dc42029
Lewis Scott <Ec2078156@edinburghcollege.ac.uk>
2024-10-15 09:49:51 +0100

    added country list functions to the one class to simplify code

--- README.md
+++ README.md
@@ -4,4 +4,3 @@
 * Develop Branch Status ![GitHub Workflow Status (branch)](https://img.shields.io/github/actions/workflow/status/LewSco/sem38/.github/workflows/main.yml?branch=develop)
 * License [![LICENSE](https://img.shields.io/github/license/LewSco/sem38?style=flat-square)](https://github.com/LewSco/sem38/blob/main/LICENSE)
 * Release [![Releases](https://img.shields.io/github/release/LewSco/sem38/all.svg?style=flat-square)](https://github.com/LewSco/sem38/releases)
-* 
--- src/main/java/com/napier/sem38/CountryList.java
+++ src/main/java/com/napier/sem38/CountryList.java
@@ -9,7 +9,6 @@ public class CountryList
     List<String> _list; // list for storing the countries
     Database _database;
 
-
     /**
      * Class Constructor
      * Assigns the database and instantiates list.
@@ -23,7 +22,8 @@ public class CountryList
     }
 
 
-    // function for getting the list of countries in the world ordered for population
+    //region World Functions
+
     /**
      * Queries the database to find countries sorted by population.
      * Stores the results in the array list variable.
@@ -59,19 +59,97 @@ public class CountryList
 
     }
 
-    // display the list
+    /**
+     * Queries the database to find the top n most populated countries in the world.
+     * Stores the results in an array list.
+     * @param n
+     */
+    public void GetWorldList(int n)
+    {
+        _list.clear(); // clear the list of any previous queries
+
+        // set up our query statement
+        String query =
+                "SELECT Name " +
+                        "FROM country " +
+                        "ORDER BY Population DESC LIMIT " + n;
+
+        // get the results from the database
+        ResultSet results = _database.Query(query);
+
+        try
+        {
+            // this moves through the results until there are no more
+            while (results.next())
+            {
+                // add each country to the list
+                _list.add(results.getString("Name"));
+            }
+        }
+        catch(Exception exception)
+        {
+            // print error messages
+            System.out.println(exception.getMessage());
+            System.out.println("Error retrieving data from ResultSet!");
+        }
+
+    }
+
+    //endregion
+
+    //region Continent
+
+    /**
+     * Queries the database to find countries sorted by population.
+     * Stores the results in the array list variable.
+     */
+    public void GetContinentList(String continent)
+    {
+        _list.clear(); // clear the list of any previous queries
+
+        // SQL query to get countries in a continent ordered by population
+        String query = "SELECT Name, Population " +
+                "FROM country " +
+                "WHERE Continent = '" + continent + "' " +
+                "ORDER BY Population DESC";
+
+        // get the results from the database
+        ResultSet results = _database.Query(query);
+
+        try
+        {
+
+            // this moves through the results until there are no more
+            while (results.next())
+            {
+                // add each country to the list
+                _list.add(results.getString("Name"));
+            }
+        }
+        catch(Exception exception)
+        {
+            // print error messages
+            System.out.println(exception.getMessage());
+            System.out.println("Error retrieving data from ResultSet!");
+        }
+
+    }
+
+    //endregion
+
+    //region Display
+
     /**
      * Displays all elements in array list (query results if assigned)
      */
     public void Display()
     {
-
-        System.out.println("List of countries:");
-
         // seems like the java equivalent of a foreach loop
         for (String country : _list) // foreach (String country in _list)
         {
             System.out.println(country);  // Console.WriteLine(country);
         }
     }
+
+    //endregion
 }
