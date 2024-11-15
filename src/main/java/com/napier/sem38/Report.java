089d9c281fa96f7d322202917457b5e5b707aebf
Lewis Scott <Ec2078156@edinburghcollege.ac.uk>
2024-11-15 12:19:30 +0000

    Added a class for storing reports

---
+++ src/main/java/com/napier/sem38/Report.java
@@ -0,0 +1,17 @@
+package com.napier.sem38;
+
+public class Report
+{
+    Database _database;
+
+    /**
+     * Class Constructor
+     * Assigns the database.
+     * @param database reference to the database
+     */
+    public Report(Database database)
+    {
+        // store the database reference
+        _database = database;
+    }
+}
