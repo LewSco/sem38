7b26e22fe0df0675e5d8b8dbcfda8bdb9204e70b
Mat <40488557@live.napier.ac.uk>
2024-12-03 13:46:36 +0000

    Added comments to long format function

--- src/main/java/com/napier/sem38/PopulationSum.java
+++ src/main/java/com/napier/sem38/PopulationSum.java
@@ -30,15 +30,23 @@ public class PopulationSum
      */
     public String FormatLong(Long num)
     {
+        //create output string and convert long to string
         String convert = num.toString();
         String out = "";
 
+        //get the offset (e.g. if number is 20 000 the first space should be after 2 characters not 3)
         int offset = convert.length() - (int)(convert.length()/3)*3;
         offset = 3 - offset;
 
+        //make offset account for the - character
+        if (num < 0)
+            offset += 1;
+
+        //add each character to the output string and create a space every 3 characters
         for (int i = 0; i < convert.length(); i++)
         {
             out += convert.charAt(i);
+            //every 3 characters (account for offset and not placing after end of number) add space
             if (i+1 != convert.length() && (i + 1 + offset)%3 == 0)
                 out += " ";
         }
@@ -135,13 +143,6 @@ public class PopulationSum
     {
         List<String> list = new ArrayList<>();
 
-        // get string for sql query
-        /*
-        SELECT Country.Continent, SUM(citySum), SUM(Country.Population)
-        FROM (SELECT SUM(Population) as citySum, Country FROM City GROUP BY Country) c
-        JOIN Country ON c.Country = Country.Name
-        GROUP BY Country.Continent;
-        */
         String query = "SELECT country.Continent AS cont, SUM(country.Population) AS total, SUM(CountSum) AS cityPop, (SUM(country.Population) - SUM(CountSum)) as nonCityPop " +
                 "FROM (SELECT SUM(Population) as CountSum, CountryCode FROM city GROUP BY CountryCode) c " +
                 "JOIN country ON country.Code = c.CountryCode " +
